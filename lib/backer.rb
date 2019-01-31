be7a97ffb21c1e4f3a048a6b83588d89d7e2ef76
eselbeus <eselbeus@gmail.com>
2019-01-31 22:14:40 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,20 +3,16 @@ require 'pry'
 class Backer 
   attr_accessor :backed_projects, :name
   
-  @@backers = []
-  
   def initialize(name)
     @name = name
     
     @backed_projects = []
     @@backers << self
-    # binding.pry
   end
   
   def back_project(project)
     @backed_projects << project
-    @@backers << @name
-    binding.pry
+    project.backers << self
   end
   
   
